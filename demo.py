1a3b5bf9ee88933135737841ca35ba06d3e84744
Frank Etrich <etrichvfrank@gmail.com>
2023-09-13 05:41:25 +0000

    13 Sep 2023

--- demo.py
+++ demo.py
@@ -5,7 +5,8 @@ n = int(input("Enter a number"))
 
 v = 1
 while v < n+1:
-    print("The number is "+ str(v)+itsspuareis)  
+    print("The number is "+ str(v)+" its spuare is "+ str(v*v)+ " and its cube is "+ str (v**3))
+    
     v += 1
     
     
